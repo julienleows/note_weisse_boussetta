3272888c0654337422feaa535f3873096ef54ba7
julienws <julienleo.weisse@gmail.com>
2021-01-29 13:35:44 +0100

    test

--- js/notes.js
+++ js/notes.js
@@ -32,16 +32,41 @@ function Note(t, c){
  */
 let noteList = {
     /** tableau des notes */
-    listesNotes : [],
+    listeNotes : [],
     // methodes
     /** permet d'ajouter une note au tableau */
     addNote(note) {
-        return this.listesNotes.push(note)-1; // pour avoir element courant, la note
+        return this.listeNotes.push(note)-1 // pour avoir element courant, la note
     },
     /** permet de retourner la note d'indice n */
-    get(n) {return this.listesNotes[n]},
+    get(n) {return this.listeNotes[n]},
     /** permet de retourner la liste de note */
-    getList() {return this.listesNotes},
+    getList() {return this.listeNotes},
+    /** */
+    save() {
+        localStorage.removeItem('listNotes')
+        localStorage.setItem('listeNotes', JSON.stringify(noteList.getList()))
+    },
+    load() {
+        let tempo = JSON.parse(localStorage.getItem('listeNotes'))
+        if (tempo) noteList.listeNotes = tempo
+    },
+    /** */
+    edit() {
+        console.log('editer')
+        noteFormView.display()
+        let note = noteList.get(app.currentNoteIndex)
+        note.titre = document.querySelector("#form_add_note_title").value
+        note.contenu = document.querySelector("#form_add_note_text").value
+
+
+        noteListView.displayItem(note)
+
+        noteView.afficherNoteDansDOM(app.currentNoteIndex)
+        noteFormView.hide() // rendre invisible le formulaire
+        noteList.save()
+        noteListView.selectItem(app.currentNoteIndex) // affichage note courante
+    }
 }
 
 
@@ -54,36 +79,36 @@ let noteList = {
 let noteListView = {
   // methodes
   selectNote(event) {
-      let elementSource = event.target;
+      let elementSource = event.target
 
-      let divChildren = document.getElementById('noteListView').children;
+      let divChildren = document.getElementById('noteListView').children
       for (let i = 0; i < divChildren.length; i++) {
-          divChildren[i].classList.remove("note_item-selected");
+          divChildren[i].classList.remove("note_item-selected")
           if (divChildren[i] === elementSource){
-              app.currentNoteIndex = i;
-              elementSource.classList.add("note_item-selected");
+              app.currentNoteIndex = i
+              elementSource.classList.add("note_item-selected")
           }
       }
       noteListView.selectItem(app.currentNoteIndex)
-      noteView.afficherNoteDansDOM(app.currentNoteIndex);
+      noteView.afficherNoteDansDOM(app.currentNoteIndex)
   },
 
   /** recoit une note et ajoute un item dans la liste affichee */
   displayItem(note) { // TODO affichage format date
-      let text = `${note.titre}    (${note.date_creation.toDateString()})`
-      let div = document.createElement('div');
-      div.classList.add("note_item");
-      div.appendChild(document.createTextNode(text));
-      div.addEventListener("click",this.selectNote);
-      document.getElementById('noteListView').appendChild(div);
+      let text = `${note.titre}    (${note.date_creation})`
+      let div = document.createElement('div')
+      div.classList.add("note_item")
+      div.appendChild(document.createTextNode(text))
+      div.addEventListener("click",this.selectNote)
+      document.getElementById('noteListView').appendChild(div)
   },
 
   selectItem(index) {
-      let divChildren = document.getElementById('noteListView').children;
+      let divChildren = document.getElementById('noteListView').children
       for (let i = 0; i < divChildren.length; i++) {
           divChildren[i].classList.remove("note_list_item-selected")
           if (i === index){
-              divChildren[i].classList.add("note_list_item-selected");
+              divChildren[i].classList.add("note_list_item-selected")
           }
       }
   }
@@ -100,8 +125,8 @@ let noteFormView = {
     // methodes
     /** rend visible le formulaire en modifiant la liste de classes css */
     display() {
-        document.getElementById('form_add_note_title').value = null;
-        document.getElementById('form_add_note_text').value = null;
+        document.getElementById('form_add_note_title').value = null
+        document.getElementById('form_add_note_text').value = null
         document.querySelector('#noteForm').classList.remove('create_edit_note-hidden')
 
     },
@@ -120,9 +145,10 @@ let noteFormView = {
 
         app.currentNoteIndex = noteList.addNote(note) // ajout de la note dans la listeNotes + mises a jour de la note courante
         noteListView.displayItem(note)
-        noteListView.selectItem(app.currentNoteIndex) // affichage note courante
+
         noteView.afficherNoteDansDOM(app.currentNoteIndex)
-        noteFormView.hide() // rendre invisible le formulaire
+        noteList.save()
+        noteListView.selectItem(app.currentNoteIndex) // affichage note courante
     }
 }
 
@@ -138,9 +164,10 @@ let noteView = {
     // methodes
     /** conversion markdown en html */
     conversionMarkdownToHtml(note) {
+        let date = note.date_creation.toString()
         let markdownText = `# ${note.titre.toUpperCase()}
 
-[ ${note.date_creation.toDateString()} ]
+[ ${date} ]
 
 ${note.contenu}`
         let conv = new showdown.Converter()
@@ -173,6 +200,16 @@ let mainMenuView = {
         document.querySelector("#form_add_note_valid").addEventListener('click', noteFormView.validate)
         // bouton 'annuler'
         document.querySelector("#form_undo").addEventListener('click', noteFormView.hide)
+        // bouton 'editer'
+        document.querySelector("#edit").addEventListener('click', noteList.edit)
+
+        // chargement des notes si localstorage
+        noteList.load()
+        if (noteList.getList()) {
+            noteList.getList().forEach((note, i) => {
+                noteListView.displayItem(note, i)
+            })
+        }
 
     }
 }
